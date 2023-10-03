84ccbcebff37377c5598102884182d7c70df4d7e
ashrafatef <ashraaf.ateef@gmail.com>
2023-10-03 18:26:06 +0200

    update log

--- infra/main.tf
+++ infra/main.tf
@@ -1,20 +1,14 @@
-provider "google" {
-  project     = var.project_id
-  region      = var.region
-  credentials = file("./cred-key.json")
-}
 
-# resource "google_service_account" "default" {
-#   account_id   = "954868622910-compute@developer.gserviceaccount.com"
-#   display_name = "Service Account"
-# }
 
-resource "google_container_cluster" "primary" {
-  name               = "terraform-node-cluster"
-  location           = var.region
+
+
+
+resource "google_container_cluster" "node-cluster" {
+  name     = "terraform-node-cluster"
+  location = var.region
 
   node_pool {
-    name = "default-pool"
+    name               = "default-pool"
     initial_node_count = 1
   }
 }
@@ -31,17 +25,7 @@ resource "google_storage_bucket_iam_member" "owner" {
   member = "user:ashrafatef.de@gmail.com"
 }
 
-data "google_client_config" "default" {}
-data "google_container_cluster" "my_cluster" {
-  name     = "terraform-node-cluster"
-  location = "EU"
-}
 
-provider "kubernetes" {
-  host                   = "https://${data.google_container_cluster.my_cluster.endpoint}"
-  token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate)
-}
 
 resource "kubernetes_deployment_v1" "my_deployment" {
   metadata {
@@ -69,7 +53,7 @@ resource "kubernetes_deployment_v1" "my_deployment" {
           name  = "my-node-app"
           image = "eu.gcr.io/terraformpractice-400622/node-server:latest" #  Replace with your Docker image
           port {
-            container_port = 8080
+            container_port = 3000
           }
         }
       }
@@ -78,9 +62,9 @@ resource "kubernetes_deployment_v1" "my_deployment" {
 }
 
 
-resource "kubernetes_service" "my_service" {
+resource "kubernetes_service_v1" "my_service" {
   metadata {
-    name = "my-node-app-service"
+    name = "my-node-app-service-v1"
   }
 
   spec {
@@ -89,8 +73,8 @@ resource "kubernetes_service" "my_service" {
     }
 
     port {
-      protocol   = "TCP"
-      port       = 80
+      protocol    = "TCP"
+      port        = 80
       target_port = 8080
     }
 
@@ -99,30 +83,30 @@ resource "kubernetes_service" "my_service" {
 }
 
 
-# resource "tls_private_key" "self_signed_key" {
-#   algorithm = "RSA"
-#   rsa_bits  = 4096
-# }
+resource "tls_private_key" "self_signed_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
 
-# resource "tls_self_signed_cert" "self_signed_cert" {
-#   private_key_pem = tls_private_key.self_signed_key.private_key_pem
+resource "tls_self_signed_cert" "self_signed_cert" {
+  private_key_pem = tls_private_key.self_signed_key.private_key_pem
 
-#   validity_period_hours = 12
-#   allowed_uses = []
-# }
-# resource "kubernetes_secret" "tls_secret" {
-#   metadata {
-#     name = "my-tls-secret"
-#   }
+  validity_period_hours = 12
+  allowed_uses = []
+}
+resource "kubernetes_secret_v1" "tls_secret" {
+  metadata {
+    name = "my-tls-secret-v1"
+  }
 
-#   data = {
-#     crt = tls_self_signed_cert.self_signed_cert.cert_pem
-#     key = tls_private_key.self_signed_key.private_key_pem
-#   }
-# }
+  data = {
+    crt = tls_self_signed_cert.self_signed_cert.cert_pem
+    key = tls_private_key.self_signed_key.private_key_pem
+  }
+}
 
 
-resource "kubernetes_ingress" "my_ingress" {
+resource "kubernetes_ingress_v1" "my_ingress" {
   metadata {
     name = "my-node-app-ingress"
   }
@@ -132,16 +116,20 @@ resource "kubernetes_ingress" "my_ingress" {
       http {
         path {
           backend {
-            service_name = kubernetes_service.my_service.metadata[0].name
-            service_port = 80
+            service {
+              name = kubernetes_service_v1.my_service.metadata[0].name
+              port {
+                  number = 80
+              }
+            }  
           }
         }
       }
     }
 
-    # tls {
-    #   secret_name = kubernetes_secret.tls_secret.metadata[0].name
-    # }
+    tls {
+      secret_name = kubernetes_secret_v1.tls_secret.metadata[0].name
+    }
   }
 }
 
